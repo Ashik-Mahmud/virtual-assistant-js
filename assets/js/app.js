52c39cc7e36dd6cafe3e811e935acd9ecd56ddcc
ashikmahmud <ashikmamud187@gmail.com>
2022-03-05 02:01:01 +0600

    added new features & command promt for alexa

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,13 +38,16 @@ const alexResponse = (text) => {
         speech.text = 'Yeah! how can i help you';
     } else if (text == 'hey Alexa please open the Google' || text == 'open the Gogle') {
         speech.text = 'ok bruh! opening google';
-        window.location.href = 'https://www.google.com';
+        let location = 'https://www.google.com/';
+        window.open(location, '_blank')
     } else if (text == 'hey Alexa please open the YouTube' || text == 'open the YouTube') {
         speech.text = 'Okay opening!';
-        window.location.href = 'https://www.youtube.com/';
+        let location = 'https://www.youtube.com/';
+        window.open(location, '_blank')
     } else if (text == 'hey Alexa please open the YouTube and play a nice song' || text == 'play song on YouTube') {
         speech.text = 'oky this is my favorite one! hope you like it';
-        window.location.href = 'https://www.youtube.com/watch?v=Vqfy4ScRXFQ&list=RDMMHQooYn2OReE&index=2';
+        let location = 'https://www.youtube.com/watch?v=Vqfy4ScRXFQ&list=RDMMHQooYn2OReE&index=2';;
+        window.open(location, '_blank')
     } else if (text == "hey Alexa do you know what's the programming Hero") {
         speech.text = 'yeah i know them cool stuff they are one of the most biggest software production company in the world.';
     } else if (text == 'I am good' || text == 'yeah I am good') {
@@ -63,7 +66,8 @@ const alexResponse = (text) => {
         speech.text = 'What do you want to search put here?'
         setTimeout(() => {
             let searchTerms = prompt('Search Your Videos');
-            window.location.href = `https://www.youtube.com/results?search_query=${searchTerms}`;
+            let location = `https://www.youtube.com/results?search_query=${searchTerms}`;
+            window.open(location, '_blank')
         }, 1000)
 
     } else if (text == 'hey Alexa do you have a girlfriend') {
@@ -79,13 +83,17 @@ const alexResponse = (text) => {
         document.querySelector('html').classList.remove('theme');
     } else if (text == 'hey Alexa please go to the programming Hero website' || text == 'go to the programming Hero dashboard') {
         speech.text = "ok I am going. Programming Hero CEO Jhanker broh himself awesome & cool";
-        window.location.href = 'https://web.programming-hero.com/dashboard';
+        let location = 'https://web.programming-hero.com/dashboard';
+        window.open(location, '_blank')
+
+    } else if (text == 'ok close yourself bro' || text == 'hey Alexa close now') {
+        speech.text = "ok I am closing. Thanks for";
+        window.close();
     } else if (text == 'hey Alexa who is your founder' || text == 'hey Alexa who made you') {
         speech.text = "Yeah! my founder name is Ashik Mahmud & He is student of programming Hero batch 5. He is so hard working man. I love him.";
     } else {
         speech.text = 'Speech Recognize Failed. please keep your microphone nearest as much as possible.';
     }
-
 
 
     document.getElementById("alexText").innerText = speech.text;
